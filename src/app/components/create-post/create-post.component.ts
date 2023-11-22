9efd2096e0a5f2dc844ded4066b9413f8c8f5be1
Hidayat-ali <meerhidayat563@gmail.com>
2023-11-22 20:00:38 +0530

    add:cors

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -33,7 +33,7 @@ export class CreatePostComponent {
 
       },
       (err) => {
-        console.error('Error saving post:', err.message);
+        console.error('Error saving post:', err);
       }
     );
   }
