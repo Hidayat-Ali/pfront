0543cd860847e5c409f93c89076cdf3f07132397
Hidayat-ali <meerhidayat563@gmail.com>
2024-04-20 15:58:19 +0530

    test again

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -11,36 +11,22 @@ import { NgForm } from '@angular/forms';
 export class BlogsComponent {
 
   token: string | undefined;
-
-
-
-
   blogPost: any;
   postContent!: SafeHtml;
   constructor(private route: ActivatedRoute, private postService: PostsService, private sanitizer: DomSanitizer) { this.token = undefined; }
   ngOnInit() {
     const encodedTopicName = this.route.snapshot.paramMap.get('title');
     console.log('i am ere', encodedTopicName);
-
-
     if (encodedTopicName !== null) {
       const title = encodedTopicName ? encodedTopicName.replace(/-/g, ' ') : '';
       console.log(title);
-
-
       const Newdata = this.postService.getPostByName(title).subscribe((data) => {
         this.blogPost = data;
         console.log(data)
         this.postContent = this.sanitizer.bypassSecurityTrustHtml(this.blogPost.dec);
-
       });
-
-
-
     }
-
   }
-
   public send(form: NgForm): void {
     if (form.invalid) {
       for (const control of Object.keys(form.controls)) {
@@ -48,7 +34,6 @@ export class BlogsComponent {
       }
       return;
     }
-
     console.debug(`Token [${this.token}] generated`);
   }
 
